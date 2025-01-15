698984a9b2052c27700e264b744d2f380b2caedc
Mahak <128406559+mahak0711@users.noreply.github.com>
2025-01-15 23:12:24 +0530

    Initialize Next App

--- app/layout.js
+++ app/layout.js
@@ -23,6 +23,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <p>hii mahak!!!</p>
       </body>
     </html>
   );
